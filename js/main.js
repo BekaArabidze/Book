6da88d82bbeb883e52ee9fe056014a7095a90f97
Beka <arabson1998@gmail.com>
2018-11-24 17:34:56 +0400

    Commit section

--- js/main.js
+++ js/main.js
@@ -4,3 +4,7 @@ $('#toggle').click(function () {
     $(this).toggleClass('active');
     $('#overlay').toggleClass('open');
 });
+
+TweenMax.from('.heading-primary--main',2,{opacity:0,ease: Back.easeOut.config(1.3), x: -300 });
+TweenMax.from('.heading-primary--sub',2,{opacity:0,ease: Back.easeOut.config(1.3), x: 300 });
+// TweenMax.from('.but',2.5,{opacity:0,ease: Back.easeOut.config(1), y: 200 });

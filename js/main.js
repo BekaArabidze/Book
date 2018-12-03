8a0afc3dc2f4409cf80f7e89631cfe4d25cf7960
Beka <arabson1998@gmail.com>
2018-12-03 23:00:51 +0400

    Paragraph animation commit

--- js/main.js
+++ js/main.js
@@ -5,6 +5,14 @@ $('#toggle').click(function () {
     $('#overlay').toggleClass('open');
 });
 
+
 TweenMax.from('.heading-primary--main',2,{opacity:0,ease: Back.easeOut.config(1.3), x: -300 });
 TweenMax.from('.heading-primary--sub',2,{opacity:0,ease: Back.easeOut.config(1.3), x: 300 });
-// TweenMax.from('.but',2.5,{opacity:0,ease: Back.easeOut.config(1), y: 200 });
+
+/// Pharagraph aniamtion
+TweenMax.from('.blogers_section__grid__box__paragraph',2,{opacity:0 ,ease: Back.easeOut.config(1.1), y: 100 });
+
+// TweenMax.from('.blogers_section__grid__box__img ',2.5,{opacity:0 ,ease: Power4.easeOut, y: -500 });
+
+
+
